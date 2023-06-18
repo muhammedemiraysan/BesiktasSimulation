06ffa416dd7d8c2d0dcf61bcdb4a809442f42bd7
Muhammed Emir AYSAN <89260017+muhammedemiraysan@users.noreply.github.com>
2023-06-18 22:45:38 +0300

    Update Arm.java

--- SIM/src/main/java/frc/robot/subsystems/Arm.java
+++ SIM/src/main/java/frc/robot/subsystems/Arm.java
@@ -26,22 +26,17 @@ import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class Arm implements AutoCloseable {
-  // The P gain for the PID controller that drives this arm.
   private double m_armKp = Constants.kDefaultArmKp;
   private double m_armSetpointDegrees = Constants.kDefaultArmSetpointDegrees;
 
-  // The arm gearbox represents a gearbox containing two Vex 775pro motors.
   private final DCMotor m_armGearbox = DCMotor.getFalcon500(1);
-
-  // Standard classes for controlling our arm
+  
   private final PIDController m_controller = new PIDController(m_armKp, 0, 0);
   private final Encoder m_encoder =
       new Encoder(Constants.kEncoderAChannel, Constants.kEncoderBChannel);
   private final WPI_TalonFX _talon = new WPI_TalonFX(1);
   private final AnalogPotentiometer m_wristPot = new AnalogPotentiometer(1, 180);
-  // Simulation classes help us simulate what's going on, including gravity.
-  // This arm sim represents an arm that can travel from -75 degrees (rotated down front)
-  // to 255 degrees (rotated down in the back).
+
   private final SingleJointedArmSim m_armSim =
       new SingleJointedArmSim(
           m_armGearbox,
@@ -51,11 +46,10 @@ public class Arm implements AutoCloseable {
           Constants.kMinAngleRads,
           Constants.kMaxAngleRads,
           true,
-          VecBuilder.fill(Constants.kArmEncoderDistPerPulse) // Add noise with a std-dev of 1 tick
+          VecBuilder.fill(Constants.kArmEncoderDistPerPulse) 
           );
   private final EncoderSim m_encoderSim = new EncoderSim(m_encoder);
 
-  // Create a Mechanism2d display of an Arm with a fixed ArmTower and moving Arm.
   private final Mechanism2d m_mech2d = new Mechanism2d(60, 60);
   private final MechanismRoot2d m_armPivot = m_mech2d.getRoot("ArmPivot", 30, 30);
   private final MechanismLigament2d m_armTower =
@@ -73,38 +67,28 @@ public class Arm implements AutoCloseable {
   public Arm() {
     
     m_encoder.setDistancePerPulse(Constants.kArmEncoderDistPerPulse);
-    // Put Mechanism 2d to SmartDashboard
     SmartDashboard.putData("Arm Sim", m_mech2d);
     m_armTower.setColor(new Color8Bit(Color.kBlue));
-
-    // Set the Arm position setpoint and P constant to Preferences if the keys don't already exist
+    
     Preferences.initDouble(Constants.kArmPositionKey, m_armSetpointDegrees);
     Preferences.initDouble(Constants.kArmPKey, m_armKp);
   }
 
-  /** Update the simulation model. */
   public void simulationPeriodic() {
     m_armSetpointDegrees = m_wristPot.get();
-    // In this method, we update our simulation of what our arm is doing
-    // First, we set our "inputs" (voltages)
+
     m_armSim.setInput(_talon.get() * RobotController.getBatteryVoltage());
 
-    // Next, wey update it. The standard loop time is 20ms.
     m_armSim.update(0.020);
 
-    // Finally, we set our simulated encoder's readings and simulated battery voltage
     m_encoderSim.setDistance(m_armSim.getAngleRads());
-    // SimBattery estimates loaded battery voltages
     RoboRioSim.setVInVoltage(
         BatterySim.calculateDefaultBatteryLoadedVoltage(m_armSim.getCurrentDrawAmps()));
 
-    // Update the Mechanism Arm angle based on the simulated arm angle
     m_arm.setAngle(Units.radiansToDegrees(m_armSim.getAngleRads()));
   }
 
-  /** Load setpoint and kP from preferences. */
   public void loadPreferences() {
-    // Read Preferences for Arm setpoint and kP on entering Teleop
     m_armSetpointDegrees = Preferences.getDouble(Constants.kArmPositionKey, m_armSetpointDegrees);
     if (m_armKp != Preferences.getDouble(Constants.kArmPKey, m_armKp)) {
       m_armKp = Preferences.getDouble(Constants.kArmPKey, m_armKp);
@@ -112,7 +96,6 @@ public class Arm implements AutoCloseable {
     }
   }
 
-  /** Run the control loop to reach and maintain the setpoint from the preferences. */
   public void reachSetpoint() {
     var pidOutput =
         m_controller.calculate(
